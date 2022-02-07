aead121663beb1d454f866436cf6ec052bb3561c
hydrate <arthmorgt2@gmail.com>
2022-02-07 14:35:21 +0100

    added my main class "MeltingSnowball.java"

---
+++ src/main/java/me/iloggedin/MeltingSnowball.java
@@ -0,0 +1,15 @@
+package me.iloggedin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+public class MeltingSnowball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new SnowBallThrowListener(), this);
+
+        BukkitTask loopthroughsnowball = new LoopThroughSnowball().runTaskTimer(this, 100L, 10);
+    }
+}
+
